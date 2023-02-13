da83e6259177d9c529d860379f759ca6f1835392
dajeong <daaa_j@naver.com>
2023-02-13 22:06:17 +0900

    modal 완료

---
+++ src/components/Modal.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect } from 'react';
+
+import styled from 'styled-components';
+import { useState, useRef } from 'react';
+
+// 버튼 공통
+const OpenModalBtn = styled.button`
+    width: 150px;
+    height: 40px;
+    border: none;
+    border-radius: 20px;
+    margin-right: 10px;
+    box-shadow: 5px 5px 10px lightgrey;
+    background-color: ${(props) => props.color};
+`;
+
+// 모달창 공통
+const ModalBox = styled.div`
+    display: ${(props) => props.isOpen};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    // 위치
+    // 맨 위로
+    z-index: 999;
+    // 중앙배치
+    // absolute : 상위요소 비례해서..
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    border-radius: 20px;
+    background-color: ${(props) => props.color};
+
+    width: 300px;
+    height: 300px;
+
+    > button {
+        margin-right: 10px;
+        width: 100px;
+        height: 30px;
+        border-radius: 20px;
+        border: none;
+        cursor: pointer;
+    }
+`;
+
+// 모달창 바깥영역 어둡게..
+const ModalOutArea = styled.div`
+    display: ${(props) => props.isOpen};
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.8);
+`;
+
+function Modal() {
+    const [open, setOpen] = useState('none');
+    const [open2, setOpen2] = useState('none');
+
+    // open 클릭시
+    const openModal = (e) => {
+        if (e.target.name === 'first') {
+            // 동시에 모달창 두개 안열리게...
+            setOpen('block');
+            setOpen2('none');
+        } else {
+            setOpen('none');
+            setOpen2('block');
+        }
+    };
+    // close 클릭시
+    const closeModal = (e) => (e.target.name === 'first' ? setOpen('none') : setOpen2('none'));
+
+    // 2번 모달창 outside 클릭시 close
+    // useRef()
+    const outside = useRef();
+
+    useEffect(() => {
+        const outsideClick = (e) => {
+            if (!outside.current.contains(e.target)) {
+                setOpen2('none');
+            }
+        };
+        document.addEventListener('mousedown', outsideClick);
+    });
+
+    return (
+        <div>
+            <h1>Modal</h1>
+            <div>
+                <OpenModalBtn onClick={openModal} name={'first'} color={'gold'}>
+                    open modal
+                </OpenModalBtn>
+                <OpenModalBtn onClick={openModal} color={'beige'}>
+                    open modal
+                </OpenModalBtn>
+            </div>
+
+            {/* 모달창 */}
+            <ModalOutArea isOpen={open}>
+                <ModalBox isOpen={open} color={'#c8e074'}>
+                    <button onClick={closeModal} name={'first'}>
+                        close
+                    </button>
+                    <button>ok</button>
+                </ModalBox>
+            </ModalOutArea>
+            <ModalOutArea isOpen={open2}>
+                <ModalBox isOpen={open2} ref={outside} color={'lavender'}>
+                    <button onClick={closeModal}>close</button>
+                </ModalBox>
+            </ModalOutArea>
+        </div>
+    );
+}
+
+export default Modal;
